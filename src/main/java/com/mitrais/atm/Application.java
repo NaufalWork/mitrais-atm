4badee8817ab089800cf3b5a56d1543d3a97546e
Achmad Naufal Syafiq <Naufal_S625@mitrais.com>
2019-10-21 16:05:51 +0700

    Reverting PIN to original to comply with TC

--- src/main/java/com/mitrais/atm/Application.java
+++ src/main/java/com/mitrais/atm/Application.java
@@ -7,8 +7,8 @@ public class Application {
     public static void main(String[] args) {
         // setting up data first
         SetupData setupData = new SetupData();
-        setupData.addAccount(new Account(" John Doe", "000000", 100, 112233));
-        setupData.addAccount(new Account("Jane Doe", "000000", 30, 112244));
+        setupData.addAccount(new Account(" John Doe", "012108", 100, 112233));
+        setupData.addAccount(new Account("Jane Doe", "932012", 30, 112244));
 
         Screens screens = new Screens(setupData);
 
